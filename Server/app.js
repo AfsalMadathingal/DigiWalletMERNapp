5d59b9de95926a34fd284506f124f967f8bcb90a
afsal <madathingalafsal@gmail.com>
2024-06-03 16:33:25 +0530

    mongodb connected

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+})
 
 
 app.listen(3000, () => console.log("Server running on port 3000"));
